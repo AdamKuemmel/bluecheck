160a2d98dfbfbd83c49c4b270630e7da6a94f990
AdamKuemmel <Adamkuemmel@gmail.com>
2022-04-12 12:18:43 -0600

    likes and comments boiiiii

--- components/Post.js
+++ components/Post.js
@@ -8,8 +8,83 @@ import {
   PaperAirplaneIcon,
 } from "@heroicons/react/outline";
 import { HeartIcon as HeartIconFilled } from "@heroicons/react/solid";
+import { useSession } from "next-auth/react";
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  onSnapshot,
+  orderBy,
+  query,
+  serverTimestamp,
+  setDoc,
+} from "firebase/firestore";
+import { db } from "../firebase";
+import { useState, useEffect } from "react";
+import Moment from "react-moment";
 
 function Post({ id, username, userImg, img, caption }) {
+  const { data: session } = useSession();
+  const [comment, setComment] = useState("");
+  const [comments, setComments] = useState([]);
+  const [likes, setLikes] = useState([]);
+  const [hasLiked, setHasLiked] = useState(false);
+
+  useEffect(
+    () =>
+      onSnapshot(
+        query(
+          collection(db, "posts", id, "comments"),
+          orderBy("timestamp", "desc")
+        ),
+        (snapshot) => setComments(snapshot.docs)
+      ),
+
+    [db, id]
+  );
+
+  useEffect(
+    () =>
+      onSnapshot(collection(db, "posts", id, "likes"), (snapshot) =>
+        setLikes(snapshot.docs)
+      ),
+
+    [db, id]
+  );
+
+  useEffect(
+    () =>
+      setHasLiked(
+        likes.findIndex((like) => like.id === session?.user?.uid) !== -1
+      ),
+    [likes]
+  );
+
+  const likePost = async () => {
+    if (hasLiked) {
+      await deleteDoc(doc(db, "posts", id, "likes", session.user.uid));
+    } else {
+      await setDoc(doc(db, "posts", id, "likes", session.user.uid), {
+        username: session.user.username,
+      });
+    }
+  };
+  const sendComment = async (e) => {
+    e.preventDefault();
+
+    //
+    const commentToSend = comment;
+    setComment("");
+
+    await addDoc(collection(db, "posts", id, "comments"), {
+      comment: commentToSend,
+      username: session.user.username,
+      userImage: session.user.image,
+      timestamp: serverTimestamp(),
+    });
+  };
+  console.log(comments);
   return (
     <div className="bg-white border rounded-sm my-7">
       {/* header */}
@@ -25,14 +100,21 @@ function Post({ id, username, userImg, img, caption }) {
       {/* img */}
       <img className="object-cover w-full" src={img} />
       {/* buttons */}
-      <div className="flex justify-between px-4 pt-4">
-        <div className="flex space-x-4">
-          <HeartIcon className="btn" />
-          <ChatIcon className="btn" />
-          <PaperAirplaneIcon className="btn" />
+      {session && (
+        <div className="flex justify-between px-4 pt-4">
+          <div className="flex space-x-4">
+            {hasLiked ? (
+              <HeartIconFilled onClick={likePost} className="btn" />
+            ) : (
+              <HeartIcon onClick={likePost} className="btn" />
+            )}
+
+            <ChatIcon className="btn" />
+            <PaperAirplaneIcon className="btn" />
+          </div>
+          <BookmarkIcon className="btn" />
         </div>
-        <BookmarkIcon className="btn" />
-      </div>
+      )}
 
       {/* caption */}
       <p className="p-5 truncate">
@@ -40,18 +122,46 @@ function Post({ id, username, userImg, img, caption }) {
         {caption}
       </p>
       {/* comments */}
-
+      {comments.length > 0 && (
+        <div className="h-20 ml-10 overflow-y-scroll scrollbar-thumb-black scrollbar-thin">
+          {comments.map((comment) => (
+            <div key={comment.id} className="flex items-center mb-3 space-x-2">
+              <img
+                className="rounded-full h-7"
+                src={comment.data().userImage}
+              />
+              <p className="flex-1 text-sm">
+                <span className="font-bold">{comment.data().username} </span>
+                {comment.data().comment}
+              </p>
+              <Moment fromNow className="pr-5 text-xs">
+                {comment.data().timestamp?.toDate()}
+              </Moment>
+            </div>
+          ))}
+        </div>
+      )}
       {/* inputbox */}
-
-      <form className="flex items-center p-4">
-        <EmojiHappyIcon className="h-7" />
-        <input
-          type="text"
-          className="flex-1 border-none outline-none focus:ring-0"
-          placeholder="Add a comment..."
-        />
-        <button className="font-semibold text-blue-400">Post</button>
-      </form>
+      {session && (
+        <form className="flex items-center p-4">
+          <EmojiHappyIcon className="h-7" />
+          <input
+            type="text"
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
+            className="flex-1 border-none outline-none focus:ring-0"
+            placeholder="Add a comment..."
+          />
+          <button
+            onClick={sendComment}
+            type="submit"
+            disabled={!comment.trim()}
+            className="font-semibold text-blue-400"
+          >
+            Post
+          </button>
+        </form>
+      )}
     </div>
   );
 }
--- components/Posts.js
+++ components/Posts.js
@@ -1,5 +1,10 @@
 import React from "react";
 import Post from "./Post";
+import { useState, useEffect } from "react";
+import { onSnapshot } from "firebase/firestore";
+import { db, storage } from "../firebase";
+import { collection, orderBy, query } from "firebase/firestore";
+
 const posts = [
   {
     id: "123",
@@ -25,16 +30,30 @@ const posts = [
 ];
 
 function Posts() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(
+    () =>
+      onSnapshot(
+        query(collection(db, "posts"), orderBy("timestamp", "desc")),
+        (snapshot) => {
+          setPosts(snapshot.docs);
+        }
+      ),
+
+    [db]
+  );
+
   return (
     <div>
       {posts.map((post) => (
         <Post
           key={post.id}
           id={post.id}
-          username={post.username}
-          userImg={post.userImg}
-          img={post.img}
-          caption={post.caption}
+          username={post.data().username}
+          userImg={post.data().profileImg}
+          img={post.data().image}
+          caption={post.data().caption}
         />
       ))}
     </div>
